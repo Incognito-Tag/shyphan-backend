a4bea8ea54677a6a66491db93610cd0961cc7e81
Akash Jaiswal <akashjaiswal3846@gmail.com>
2024-05-31 00:05:16 +0530

    server: added blank route for server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
-const apiLeads = require('./routes/leads');
-const apiUsers = require('./routes/users');
+const apiLeads = require("./routes/leads");
+const apiUsers = require("./routes/users");
 
 const PORT = process.env.PORT;
 const mongoURLString = process.env.DATABASE_URL;
@@ -13,23 +13,26 @@ const mongoURLString = process.env.DATABASE_URL;
 mongoose.connect(mongoURLString);
 const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-})
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+database.on("error", (error) => {
+  console.log(error);
+});
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const router = express.Router();
+router.get("/", function (req, res, next) {
+  res.send("Server is up and running fine :)");
+});
+app.use(router);
 
-app.use('/api/leads', apiLeads)
-app.use('/api/users', apiUsers)
-
+app.use("/api/leads", apiLeads);
+app.use("/api/users", apiUsers);
 
 app.listen(PORT, () => {
-    console.log(`Server Started at ${PORT}`)
-})
-
+  console.log(`Server Started at ${PORT}`);
+});
