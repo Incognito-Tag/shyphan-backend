7003b1061d74fe6fa73435cae0bd8320ddad7e30
Akash Jaiswal <akashjaiswal3846@gmail.com>
2024-06-07 12:13:55 +0530

    feat: api for getting user details

--- routes/auth.js
+++ routes/auth.js
@@ -37,4 +37,34 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/getDetails", async (req, res) => {
+  const { token } = req.body;
+
+  try {
+    const decodedToken = jwt.verify(token, secretKey);
+    const userId = decodedToken.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(200).json({ error: "User not found" });
+    }
+
+    const userDetails = {
+      _id: user._id,
+      name: user.name,
+      mobileNo: user.mobileNo,
+      email: user.email,
+      employeeId: user.employeeId,
+      employeeType: user.employeeType,
+      leads: user.leads,
+    };
+
+    res.json(userDetails);
+  } catch (error) {
+    console.error("Failed to get user details:", error);
+    res.status(500).json({ error: "Failed to get user details" });
+  }
+});
+
 module.exports = router;
